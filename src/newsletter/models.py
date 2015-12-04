4a1c64dab7f42e8844c511a5d73dbe9381a0ffb3
akopen <alexkopen@gmail.com>
2015-12-03 20:01:00 -0600

    Newsletter app models.py changes

--- src/newsletter/models.py
+++ src/newsletter/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
+
 # Create your models here.
+from django.utils.datetime_safe import datetime
+
+
+class SignUp(models.Model):
+    email = models.EmailField()
+    full_name = models.CharField(max_length=100, blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __unicode__(self):
+        return self.email
